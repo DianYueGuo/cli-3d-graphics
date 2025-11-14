b2ee0499f6684f3640fe076eb5384fec4e2aa5d4
DianYueGuo <dianyueguo@gmail.com>
2025-11-14 09:47:21 +0800

    change fstream to ifstream

--- obj.cpp
+++ obj.cpp
@@ -5,15 +5,15 @@
 
 
 void read_obj_file(const std::string &file_name) {
-    std::fstream file_stream (file_name, std::fstream::in);
+    std::ifstream input_file_stream (file_name, std::fstream::in);
 
     std::string read_line;
 
-    while (file_stream.good()) {
-        std::getline(file_stream, read_line);
+    while (input_file_stream.good()) {
+        std::getline(input_file_stream, read_line);
 
         std::cout << read_line << std::endl;
     }
 
-    file_stream.close();
+    input_file_stream.close();
 }
